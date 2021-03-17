e93cac50e0620bb482fe82e1deb1f6cec6ac07c5
쇼옴유저 <sjun1019@naver.com>
2021-03-17 17:42:47 +0900

    edited for add template

--- install.sh
+++ install.sh
@@ -3,16 +3,38 @@
 echo /////////////////////
 echo     Install vimrc
 echo /////////////////////
+echo ;
 
 cp ./vimrc ~/.vimrc
 
-echo /////////////////////
-echo     Download plug
-echo /////////////////////
+if [ -d ./templates ];then
+	if [ -e ./templates ];then
+		if [ ! -d ~/.vim/templates ]; then 
+			mkdir ~/.vim/templates
+		fi
+		cp ./templates/*.template ~/.vim/templates
+	fi
+fi
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo //////////////////////////
+echo "Install plug? (y/n)"
+echo //////////////////////////
+echo ;
+read answer
+case $answer in
+	[yY])
+		echo /////////////////////
+		echo     Download plug
+		echo /////////////////////
 
-echo /////////////////////////////////
-echo Now run vim and type :PlugInstall
-echo /////////////////////////////////
+		curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+			https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+		echo /////////////////////////////////
+		echo Now run vim and type :PlugInstall
+		echo ///////////////////////////////// ;;
+	[nN]*)
+		echo ;
+		echo Done ;;
+esac
+exit 0
